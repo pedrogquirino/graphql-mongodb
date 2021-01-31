4432317fb9cff773796b393452c74f1c0bc4b554
pedrogquirino <pedrogquirino@gmail.com>
2021-01-31 16:15:38 -0300

    Initial commit

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateLessonInput } from './lesson.input';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+
+  async getLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate, students } = createLessonInput;
+
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+      students,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentsIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentsIds];
+    return this.lessonRepository.save(lesson);
+  }
+}
